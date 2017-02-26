da29cd295824fcc652124fd5045454c7b6056d52
QVaucher <quentin.vaucher@protonmail.com>
2017-02-26 22:24:23 +0100

    Add/Remove dynamically the mantissa checkbox acording to the number of bits.

--- labo1.js
+++ labo1.js
@@ -1,9 +1,12 @@
+function onLoad() {
+  checkboxChange();
+}
 
- // Cette fonction permet d'ajouter dynamiquement les checkbox de l'exposant
+// Cette fonction permet d'ajouter dynamiquement les checkbox de l'exposant
 function exponentCheckbox() {
-  var checkboxList = document.getElementById('binaryExponent');
-  while (checkboxList.firstChild) {
-    checkboxList.removeChild(checkboxList.firstChild);
+  var exponentList = document.getElementById('binaryExponent');
+  while (exponentList.firstChild) {
+    exponentList.removeChild(exponentList.firstChild);
   }
 
   var nbBits = document.getElementsByName('nbBits')[0].value;
@@ -26,4 +29,50 @@ function exponentCheckbox() {
 
     document.getElementById('binaryExponent').appendChild(checkbox);
   }
+}
+
+// Supprime toutes les checkbox de l'id donné
+function clearCheckbox(id) {
+  var checkboxList = document.getElementById(id);
+  while (checkboxList.firstChild) {
+    checkboxList.removeChild(checkboxList.firstChild);
+  }
+}
+
+function createCheckbox(id, name, size) {
+  for (var i = 0; i < size; i++) {
+    var checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.name = name;
+    checkbox.value = "1";
+
+    document.getElementById(id).appendChild(checkbox);
+
+  }
+}
+
+function checkboxChange() {
+  clearCheckbox("binaryExponent");
+  clearCheckbox("binaryMantissa");
+
+  var nbBits = document.getElementsByName('nbBits')[0].value;
+  var exponentSize;
+
+  // Cf. page wiki anglaise de la norme IEEE754, section "interchange formats"
+  if (nbBits == 16) {
+    exponentSize = 5;
+  } else if (nbBits == 32) {
+    exponentSize = 8;
+  } else {
+    exponentSize = Math.round(4 * Math.log(nbBits) / Math.log(2) - 13);
+  }
+
+  var mantissaSize = nbBits - exponentSize - 1;
+
+  createCheckbox("binaryExponent", "exponentCheckbox", exponentSize);
+  createCheckbox("binaryMantissa", "mantissaCheckbox", mantissaSize);
+}
+
+function updateSign() {
+  
 }
